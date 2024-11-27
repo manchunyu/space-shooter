6cfe17404e9df94698b648f7da6a09696a558a0c
Man Chun Yu <182493478+YuRMyDestiny@users.noreply.github.com>
2024-11-27 20:12:09 +0000

    General set up of pygame

--- code/main.py
+++ code/main.py
@@ -1,2 +1,21 @@
-import pygame
+import sys, pygame
 
+# General Setup
+pygame.init()
+
+WINDOW_SIZE = (WINDOW_WIDTH, WINDOW_HEIGHT) = (1280, 720)
+display_surface = pygame.display.set_mode(WINDOW_SIZE)
+pygame.display.set_caption("Space Shooter")
+
+# Surface
+surf = pygame.Surface((100, 200))
+
+while True:
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    display_surface.fill("darkgray")
+    pygame.display.flip()
