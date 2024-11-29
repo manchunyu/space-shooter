2502ef5ee0bba4f5814aaba60bbc0fd550412f0d
Man-Chun Yu <brian0821yu@gmail.com>
2024-11-29 13:30:30 +0000

    Add surfaces

--- code/main.py
+++ code/main.py
@@ -10,9 +10,19 @@ display_surface = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption("Space Shooter")
 clock = pygame.time.Clock()
 
-# Surface
+# Surfaces
 
 player_surf = pygame.image.load("../images/player.png").convert_alpha()
+player_pos = [WINDOW_WIDTH / 2, WINDOW_HEIGHT/ 2]
+player_rect = player_surf.get_frect(center = player_pos)
+
+meteor_surf = pygame.image.load("../images/meteor.png").convert_alpha()
+meteor_pos = [WINDOW_WIDTH / 2, WINDOW_HEIGHT/ 2]
+meteor_rect = meteor_surf.get_frect(center = meteor_pos)
+
+laser_surf = pygame.image.load("../images/laser.png").convert_alpha()
+laser_rect = laser_surf.get_frect(bottomleft = (WINDOW_WIDTH - 20, WINDOW_HEIGHT - 20))
+
 star = pygame.image.load("../images/star.png").convert_alpha()
 star_pos = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for _ in range(20)]
 
@@ -22,14 +32,13 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    
-    
+
     display_surface.fill("darkgray")
     
     for pos in star_pos:
         display_surface.blit(star, pos)
-
-    display_surface.blit(player_surf, (0, 0))
+    player_rect.bottom += 1
+    display_surface.blit(player_surf, player_rect)
    
     pygame.display.flip()
 
