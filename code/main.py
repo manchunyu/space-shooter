8cc2f8a6b15242c14ad14ea531c6f5ea219a22d0
Man-Chun Yu <brian0821yu@gmail.com>
2024-11-29 14:19:14 +0000

    Add meteor, laser, and player movement

--- code/main.py
+++ code/main.py
@@ -15,16 +15,18 @@ clock = pygame.time.Clock()
 player_surf = pygame.image.load("../images/player.png").convert_alpha()
 player_pos = [WINDOW_WIDTH / 2, WINDOW_HEIGHT/ 2]
 player_rect = player_surf.get_frect(center = player_pos)
+player_dir = 1
 
 meteor_surf = pygame.image.load("../images/meteor.png").convert_alpha()
 meteor_pos = [WINDOW_WIDTH / 2, WINDOW_HEIGHT/ 2]
 meteor_rect = meteor_surf.get_frect(center = meteor_pos)
 
 laser_surf = pygame.image.load("../images/laser.png").convert_alpha()
-laser_rect = laser_surf.get_frect(bottomleft = (WINDOW_WIDTH - 20, WINDOW_HEIGHT - 20))
+laser_rect = laser_surf.get_frect(bottomleft = (20, WINDOW_HEIGHT - 20))
 
 star = pygame.image.load("../images/star.png").convert_alpha()
 star_pos = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for _ in range(20)]
+
 
 while True:
     
@@ -37,8 +39,19 @@ while True:
     
     for pos in star_pos:
         display_surface.blit(star, pos)
-    player_rect.bottom += 1
+    
+    display_surface.blit(meteor_surf, meteor_rect)
+    display_surface.blit(laser_surf, laser_rect)
+
     display_surface.blit(player_surf, player_rect)
+    
+    
+
+    if player_rect.right >= WINDOW_WIDTH or player_rect.left <= 0:
+        player_dir *= -1
+
+    player_rect.right += player_dir
+    
    
     pygame.display.flip()
 
